abb2cc46e2c53c7f1d68d222a2292f54e6e3b8ce
ElenaGrasovskaya <elenagrasovskaya@gmail.com>
2021-05-17 12:07:14 +0300

    Added Recent Works and Project sections

--- src/pages/index.11ty.js
+++ src/pages/index.11ty.js
@@ -74,6 +74,63 @@ module.exports = class {
       </div>
 
       <h2 class="big-title"><span>Projects</span></h2>
+      <div class="projects-section">
+          <div class="projects-section-image">
+          ${await this.image('svg/project1.svg')}
+          <div class="logo-list"> ${await this.image('svg/mashable.svg')}  ${await this.image('svg/kickstarter.svg')}  ${await this.image('svg/huffpost.svg')}</div>
+            
+          </div>
+            <div class="projects-section-text">
+              <p class="thin-grey-text">UX, UI, Illustrations, Icons</p>
+              <p class="black-text">Reinvented bedding cutting price for 50%</p>
+              <p><a class="mail-link" href="https://www.behance.net/gallery/65940547/Primary-Smart-Bedding-Website" rel="noopener noreferrer"><span>more info</span></a></p>
+              <div class="portrait-container">  
+                <div class="portrait">${await this.image('svg/face1.svg')}</div>
+                <div><span class="black-text">Unwavering expertise!</span><br>
+                    <span class="thin-grey-text">Marshall Haas, CEO</span>
+                  </div>
+              </div>
+            </div>
+      </div>
+      <div class="projects-section-reverse">
+          
+            <div class="projects-section-text-reverse">
+              <p class="thin-grey-text">Analytics, UX, UI, Icons, Front-end</p>
+              <p class="black-text">Game Analytics wins TechCrunch Disrupt & Raise $2.6M</p>
+              <p><a class="mail-link" href="https://www.behance.net/gallery/65940547/Primary-Smart-Bedding-Website" rel="noopener noreferrer"><span>more info</span></a></p>
+              <div class="portrait-container">
+                <div class="portrait">${await this.image('svg/face2.svg')}</div>
+                <div><span class="black-text">Pro and creative!</span><br>
+                    <span class="thin-grey-text">Bogdan Suchyk, CEO</span>
+                  </div>
+              </div>
+
+
+            </div>
+            <div class="projects-section-image-reverse">
+              ${await this.image('svg/project2.svg')}
+              <div class="logo-list"> ${await this.image('svg/forbes.svg')}  ${await this.image('svg/techcrunch.svg')}  ${await this.image('svg/vc.ru.svg')} ${await this.image('svg/angellist.svg')}</div>
+            
+            </div>
+      </div>
+      <div class="projects-section">
+          <div class="projects-section-image">
+          ${await this.image('svg/project3.svg')}
+          <div class="logo-list"> ${await this.image('svg/forbes.svg')}  ${await this.image('svg/techcrunch.svg')}  ${await this.image('svg/mashable.svg')} ${await this.image('svg/indiegogo.svg')}</div>
+            
+          </div>
+            <div class="projects-section-text">
+              <p class="thin-grey-text">Analytics, UX, UI, Front-end</p>
+              <p class="black-text">Indiegogo superstar smart clock which ease your life</p>
+              <p><a class="mail-link" href="https://www.behance.net/gallery/65940547/Primary-Smart-Bedding-Website" rel="noopener noreferrer"><span>more info</span></a></p>
+              <div class="portrait-container">
+                <div class="portrait">${await this.image('svg/face3.svg')}</div>
+                <div><span class="black-text">They know what to do!</span><br>
+                    <span class="thin-grey-text">Anton Glance, CEO</span>
+                  </div>
+              </div>
+            </div>
+      </div>
 
       
       
