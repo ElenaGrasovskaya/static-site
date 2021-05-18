6b952aaaf29b1f3fed6c4ffe6110ef0b933725ec
Kapelianovych Yevhen <kapelianovych.y.v@gmail.com>
2021-05-18 10:50:45 +0300

    Make size links sifferent.

--- src/components/header/components/menu.js
+++ src/components/header/components/menu.js
@@ -1,22 +1,22 @@
 module.exports = async function menu() {
   return /* html */ `
     <aside class="menu">
-      <a class="menu-link" data-icon="projects" href="/projects">
+      <a class="menu-link projects" data-icon="projects" href="/projects">
         <div class="background-image"></div>
         <span></span>
         <span>projects</span>
       </a>   
-      <a class="menu-link" data-icon="services" href="/services">
+      <a class="menu-link shrink services" data-icon="services" href="/services">
         <div class="background-image"></div>
         <span></span>
         <span>services</span>
       </a>   
-      <a class="menu-link" data-icon="blog" href="/blog">
+      <a class="menu-link shrink blog" data-icon="blog" href="/blog">
         <div class="background-image"></div>
         <span></span>
         <span>blog</span>
       </a>   
-      <a class="menu-link" data-icon="contacts" href="/contacts">
+      <a class="menu-link semi-shrink contacts" data-icon="contacts" href="/contacts">
         <div class="background-image"></div>
         <span></span>
         <span>contacts</span>
